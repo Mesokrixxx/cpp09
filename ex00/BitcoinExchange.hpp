98647fba3b8cbf6595b958ab1ba64c2a5b395ce6
AFlandin <fldnadam@gmail.com>
2026-03-19 11:29:22 +0100

    dsq

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class BitcoinExchange {
+public:
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &other);
+    BitcoinExchange& operator=(const BitcoinExchange &other);
+    ~BitcoinExchange();
+
+private:
+    
+};
---
+++ ex00/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "usage: " << argv[0] << " <inputfile>" << std::endl;
+        return 0;
+    }
+    return 0;
+}
